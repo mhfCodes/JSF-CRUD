b45a962c12c8f9a1521e452ca8deb26fc64467bd
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-08-26 00:45:25 +0430

    added task entity

---
+++ src/main/java/models/Tasks.java
@@ -0,0 +1,57 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TASKS")
+public class Tasks {
+
+	@Id
+	@SequenceGenerator(
+				name = "seq_tasks",
+				sequenceName = "seq_tasks",
+				allocationSize = 1
+			)
+	@GeneratedValue(
+				strategy = GenerationType.SEQUENCE,
+				generator = "seq_tasks"
+			)
+	private Long id;
+	
+	@Column(name = "TASK_BODY")
+	private String taskBody;
+	
+	@Column(name = "COMPLETED")
+	private Boolean completed;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTaskBody() {
+		return taskBody;
+	}
+
+	public void setTaskBody(String taskBody) {
+		this.taskBody = taskBody;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+	
+}
